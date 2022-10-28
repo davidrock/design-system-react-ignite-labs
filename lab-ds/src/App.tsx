87470c8bad2ee0aff04542be482b5b5f8d9b7ff9
David Martinelli <david.martinelli@farfetch.com>
2022-10-28 20:12:12 +0100

    create Button component and Stories

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from './components/Button';
+
 export function App() {
   return (
     <div className="p-8">
-      <h1 className='text-white'>Hello darknes</h1>
-      <button className="bg-cyan-500 hover:bg-cyan-300 transition duration-700 px-4 py-2 rounded">Enviar</button>
+      <h1 className="text-white">Hello darknes</h1>
+      <button className="bg-cyan-500 hover:bg-cyan-300 transition duration-700 px-4 py-2 rounded">
+        Enviar
+      </button>
+
+      <Button>Teste</Button>
     </div>
   );
 }
