8b99052a5583de4f4c020340a5a239931384fbaf
David Martinelli <david.martinelli@farfetch.com>
2022-10-28 21:29:15 +0100

    create TextInput component and Stories

---
+++ lab-ds/src/components/TextInput.tsx
@@ -0,0 +1,15 @@
+import { clsx } from 'clsx';
+import { InputHTMLAttributes, ReactNode } from 'react';
+
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function TextInput(props: TextInputProps) {
+  return (
+    <input
+      className={clsx(
+        'py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400 w-full'
+      )}
+      {...props}
+    />
+  );
+}
--- lab-ds/tailwind.config.cjs
+++ lab-ds/tailwind.config.cjs
@@ -13,14 +13,17 @@ module.exports = {
     colors: {
       white: '#FFF',
       black: '#000',
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7c7c8a',
-      'gray-200': '#c4c4cc',
-      'gray-100': '#e1e1e6',
-
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#A1E6FF',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+      cyan: {
+        500: '#81D8F7',
+        300: '#A1E6FF',
+      },
     },
     extend: {
       fontFamily: {
