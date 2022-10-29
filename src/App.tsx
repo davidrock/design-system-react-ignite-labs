899921609096efd46ad900f7b652f17f64eb43b0
David Martinelli <david.martinelli@farfetch.com>
2022-10-29 12:58:41 +0100

    improve components and create index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
+import { Envelope, Fingerprint, Key } from 'phosphor-react';
 import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
 
 export function App() {
   return (
-    <div className="p-8">
-      <h1 className="text-white">Hello darknes</h1>
-      <button className="bg-cyan-500 hover:bg-cyan-300 transition duration-700 px-4 py-2 rounded">
-        Enviar
-      </button>
+    <div className="flex flex-col items-center justify-center w-screen h-screen text-gray-100 bg-gray-900">
+      <header className="flex flex-col items-center">
+        <Logo />
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+        <Text size="lg" className="mt-1 text-gray-400">
+          Sign in and start using
+        </Text>
+      </header>
 
-      <Button>Teste</Button>
+      <form className="flex flex-col items-stretch w-full max-w-sm gap-4 mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Your e-mail address</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input
+              id="email"
+              type="email"
+              placeholder="Digite seu e-mail"
+            ></TextInput.Input>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Your password</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Fingerprint />
+            </TextInput.Icon>
+            <TextInput.Input
+              id="password"
+              type="password"
+              placeholder="Your password"
+            ></TextInput.Input>
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">
+            Remember me for 30 days
+          </Text>
+        </label>
+
+        <Button type="submit" className="mt-4">
+          Sign in
+        </Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline">
+            I forgot my password!
+          </a>
+        </Text>
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline">
+            Don't have an account? Create one now!
+          </a>
+        </Text>
+      </footer>
     </div>
   );
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,21 +5,25 @@ import { ReactNode } from 'react';
 export interface TextProps {
   children: ReactNode;
   size?: 'xs' | 'sm' | 'md' | 'lg';
-  asChild?: boolean
+  asChild?: boolean;
   className?: string;
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : 'span';
 
   return (
     <Comp
-      className={clsx('text-gray-100 font-sans', {
-        'text-xs': size === 'xs',
-        'text-sm': size === 'sm',
-        'text-md': size === 'md',
-        'text-lg': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'xs',
+          'text-sm': size === 'sm',
+          'text-md': size === 'md',
+          'text-lg': size === 'lg',
+        },
+        className
+      )}
     >
       {children}
     </Comp>
